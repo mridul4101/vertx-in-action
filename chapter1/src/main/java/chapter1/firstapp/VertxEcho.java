9629843f857553b9dfd2b90775a04a47c75c910c
mridul4101 <mridul.dgp2015@gmail.com>
2023-02-12 14:14:59 +0530

    changed indentation

--- chapter1/src/main/java/chapter1/firstapp/VertxEcho.java
+++ chapter1/src/main/java/chapter1/firstapp/VertxEcho.java
@@ -5,34 +5,34 @@ import io.vertx.core.net.NetSocket;
 
 public class VertxEcho {
 
-  private static int numberOfConnections = 0;
+    private static int numberOfConnections = 0;
 
-  public static void main(String[] args) {
-    Vertx vertx = Vertx.vertx();
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
 
-    vertx.createNetServer()
-      .connectHandler(VertxEcho::handleNewClient)
-      .listen(3000);
+        vertx.createNetServer()
+            .connectHandler(VertxEcho::handleNewClient)
+            .listen(3000);
 
-    vertx.setPeriodic(5000, id -> System.out.println(howMany()));
+        vertx.setPeriodic(5000, id -> System.out.println(howMany()));
 
-    vertx.createHttpServer()
-      .requestHandler(request -> request.response().end(howMany()))
-      .listen(8080);
-  }
+        vertx.createHttpServer()
+            .requestHandler(request -> request.response().end(howMany()))
+            .listen(8080);
+    }
 
-  private static void handleNewClient(NetSocket socket) {
-    numberOfConnections++;
-    socket.handler(buffer -> {
-      socket.write(buffer);
-      if (buffer.toString().endsWith("/quit\n")) {
-        socket.close();
-      }
-    });
-    socket.closeHandler(v -> numberOfConnections--);
-  }
+    private static void handleNewClient(NetSocket socket) {
+        numberOfConnections++;
+        socket.handler(buffer -> {
+            socket.write(buffer);
+            if (buffer.toString().endsWith("/quit\n")) {
+                socket.close();
+            }
+        });
+        socket.closeHandler(v -> numberOfConnections--);
+    }
 
-  private static String howMany() {
-    return "We now have " + numberOfConnections + " connections";
-  }
+    private static String howMany() {
+        return "We now have " + numberOfConnections + " connections";
+    }
 }
